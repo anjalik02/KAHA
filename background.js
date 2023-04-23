b158fa4c63a8f56ee708687ab0e7bca122095f20
kirthivel-ramesh <kirthivel.ramesh@tamu.edu>
2023-04-23 01:32:27 -0500

    final update

--- background.js
+++ background.js
@@ -2,3 +2,46 @@ chrome.runtime.onInstalled.addListener(() => {
     chrome.action.setPopup({ popup: 'index.html' });
   });
   
+  // Get the currently playing song from the Spotify API
+  async function getCurrentlyPlayingSong(accessToken) {
+    const response = await fetch('https://api.spotify.com/v1/me/player/currently-playing', {
+      headers: {
+        'Authorization': `Bearer ${accessToken}`
+      }
+    });
+  
+    if (response.ok) {
+      const song = await response.json();
+      return song;
+    } else {
+      throw new Error('Failed to fetch currently playing song');
+    }
+  }
+  
+  // Get the auth token
+  function getAuthToken() {
+    return new Promise((resolve, reject) => {
+      chrome.identity.getAuthToken({ interactive: true }, token => {
+        if (chrome.runtime.lastError) {
+          reject(chrome.runtime.lastError);
+        } else {
+          resolve(token);
+        }
+      });
+    });
+  }
+  
+  chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
+    if (request.action === 'authenticateAndGetSong') {
+      try {
+        const token = await getAuthToken();
+        const song = await getCurrentlyPlayingSong(token);
+        sendResponse({ success: true, song: song });
+      } catch (error) {
+        console.error(error);
+        sendResponse({ success: false, error: error.message });
+      }
+    }
+    return true;
+  });
+  
--- script.js
+++ script.js
@@ -1,23 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-    // Get the currently playing song from the Spotify API
-    function getCurrentlyPlayingSong(accessToken) {
-      return new Promise((resolve, reject) => {
-        const xhr = new XMLHttpRequest();
-        xhr.open('GET', 'https://api.spotify.com/v1/me/player/currently-playing');
-        xhr.setRequestHeader('Authorization', `Bearer ${accessToken}`);
-        xhr.onload = () => {
-          if (xhr.status === 200) {
-            const song = JSON.parse(xhr.responseText);
-            resolve(song);
-          } else {
-            reject(new Error('Failed to fetch currently playing song'));
-          }
-        };
-        xhr.send();
-      });
-    }
-  
     // Convert the song to Braille sheet music
     async function convertToBrailleSheetMusic(song) {
       // Use a music notation API to convert the song to sheet music
@@ -27,31 +9,18 @@ document.addEventListener('DOMContentLoaded', function () {
       brailleSheetMusicDiv.textContent = 'Braille sheet music for the song would be displayed here';
     }
   
-    // Get the auth token
-    function getAuthToken() {
-      return new Promise((resolve, reject) => {
-        chrome.identity.getAuthToken({ interactive: true }, token => {
-          if (chrome.runtime.lastError) {
-            reject(chrome.runtime.lastError);
-          } else {
-            resolve(token);
-          }
-        });
+    // Fetch the song from Spotify
+    async function fetchSongFromSpotify() {
+      chrome.runtime.sendMessage({ action: 'authenticateAndGetSong' }, response => {
+        if (response.success) {
+          convertToBrailleSheetMusic(response.song);
+        } else {
+          console.error(response.error);
+        }
       });
     }
   
-    // Authenticate and fetch the song from Spotify
-    async function authenticateAndGetSong() {
-      try {
-        const token = await getAuthToken();
-        const song = await getCurrentlyPlayingSong(token);
-        await convertToBrailleSheetMusic(song);
-      } catch (error) {
-        console.error(error);
-      }
-    }
-  
     // Call the function on popup load
-    authenticateAndGetSong();
+    fetchSongFromSpotify();
   });
   
