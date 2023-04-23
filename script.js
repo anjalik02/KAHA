caf1aa42aa94d5ae62189a8b087726068928db1e
“hdhulipala02” <hdhulipala02@tamu.edu>
2023-04-23 16:08:48 -0500

    get lyrics from genius

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-chrome.runtime.sendMessage({name: "fetchWords"}, (response) =>{
-  console.log(response);
+// const url = window.location.href;
+// if (window.location.hostname !== "genius.com") {
+//   const lyricsContainer = document.getElementById("lyrics-container");
+//     lyricsContainer.innerText = "";
+// }
 
-  //Updating the Word with the name of the song
-  document.querySelector('#songName').innerHTML = response.items[0].snippet.title;
+chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+  var currentUrl = tabs[0].url;
+  console.log("Current URL: " + currentUrl);
+const lyricsClass = "Lyrics__Container-sc-1ynbvzw-5";
 
+fetch(currentUrl)
+  .then(response => response.text())
+  .then(html => {
+    const doc = new DOMParser().parseFromString(html, "text/html");
+    const lyricsElem = doc.querySelector(`div.${lyricsClass}`);
+    const lyricsText = lyricsElem.innerText.trim();
+    const lyricsContainer = document.getElementById("lyrics-container");
+    lyricsContainer.innerText = lyricsText;
+  })
+  .catch(error => console.error(error));
 });
-
